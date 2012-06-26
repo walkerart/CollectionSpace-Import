671ed97aed3f581f9b33b8df03e6b2354cc1f6a4
Nate Solas <homebrewer@gmail.com>
2012-06-26 16:21:38 -0500

    big rewrite of the psql dump so it's in multiple tables

--- tab_to_psql.py
+++ tab_to_psql.py
@@ -21,7 +21,7 @@ fout = codecs.open( "/tmp/out1.sql", "wt", "utf-8" )
 
 #establish a connection with db
 try:
-    conn = psycopg2.connect("dbname='%s' user='%s'" % ('ulan','nschroeder'))
+    conn = psycopg2.connect("dbname='%s' user='%s' host='%s'" % ('cnr','nschroeder','colbert.walkerart.org'))
     #print conn.encoding
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 except Exception as e:
@@ -30,14 +30,50 @@ except Exception as e:
     exit()
 
 ACC_COL = 35
+OBJECTID_COL = 38
+TITLE_COL = 41
+FIRSTNAME_COL=78
+LASTNAME_COL=69
 
 sql = u''
-sql_cols = ''
-sql_placeholders = ''
+wac_obj_placeholders = ''
 size = 0
 limit = 1000000
 count = 0
-working_cols = []
+objectid = ''
+full_col_list = []
+wac_object_col_list = []
+related_tables = {'wac_condition':[
+                               'Condition',
+                               'ConditionDate'
+                               ],
+                  'wac_artist':[
+                            'Artist',
+                            'birthdate',
+                            'deathdate',
+                            'FirstName',
+                            'LastName',
+                            'preferredlabel',
+                            'role',
+                            'ULANID',
+                            'ulan_ulan_nationality',
+                            ],
+                  'wac_valuation':[
+                               'ValuationDate',
+                               'Valuationsource',
+                               'CurrentValue'
+                               ],
+                  'wac_dimensions':[
+                               'Width',
+                               'Height',
+                               'Depth',
+                               'Weight',
+                               'DimDescription',
+                               'Dimensions',
+                               ],
+                  }
+create_tables = {}
+create_data = {}
 
 for line in fin:
     count+=1
@@ -45,55 +81,97 @@ for line in fin:
         #line = unicode( line, "utf-8" )
         cols = line.split('\t')
         cols[-1:] = [cols[-1:][0].strip()]
+        sys.stderr.write("line {}\n".format(str(count)))
         if count == 1:
             col_list = []
             # first row is the field names, spit it out
-            sql = u"""DROP TABLE if exists wac_object;
-    CREATE TABLE wac_object (
-    """
+            sql = u""
+            
             for col in cols:
                 col = col.replace(' ','').replace('.','').replace(':','').replace('/','').replace('#','').replace("'",'')
                 col = col.replace('nationality','ulan_nationality') # bug in wacart I can't correct till Joe's done
                 if col == 'Date':
                     col = 'date_wac'
                 col = col.replace('PrinterøsMarks','PrintersMarks') # bug in wacart I can't correct till Joe's done
-                col_list.append("{}".format(col))
-                sql += u"    {} text,\n".format(col)
-            sql = sql[:-2] # remove trailing comma
-            sql += u"""
-    );
-    """
-            size = len(col_list)
-            sql_cols = ','.join(col_list)
-            sql_placeholders = '\t'.join(['%s' for fake in col_list])
+                full_col_list.append("{}".format(col))
+                wac_object = True
+                for key, val in related_tables.items():
+                    if col in val:
+                        wac_object = False
+                        if key not in create_tables:
+                            create_tables[key] = ['wac_object_id']
+                            if key == 'wac_artist':
+                                create_tables[key].append('web_id')
+                        create_tables[key].append(col)
+                        #sys.stderr.write("SKIP %s\n" % (col))
+                        # store these in order, I reckon, so we can create the table and store the pieces
+                if wac_object:
+                    if 'wac_object' not in create_tables:
+                        create_tables['wac_object'] = ['id','web_id']
+                    create_tables['wac_object'].append(col)
+                    
+                    wac_object_col_list.append("{}".format(col))
+                    
+                    
             
-            sql += u"COPY wac_object ({}) FROM stdin;\n".format(sql_cols)
+            # generate the sql
+            for tablename,tablecols in create_tables.items():
+                sql += u"""DROP TABLE if exists {};
+CREATE TABLE {} (\n""".format(tablename,tablename)
+                for tablecol in tablecols:
+                    sql += u"    {} text,\n".format(tablecol)
+                sql = sql[:-2] # remove trailing comma
+                sql += u"""
+);\n"""
+#            size = len(col_list)
+            
+            
         else:
-            if len(cols) < size:
-                cols.insert(size-8,'') # bug in wacart I can't correct till Joe's done
-            if cols[ACC_COL] and count > 1:
-                out_cols = []
-                for c in working_cols:
-                    out_cols.append("|||".join(c))
-                if len(out_cols) > 0:
-                    sql += u"\t".join(out_cols)+"\n"
-                working_cols = [[c] for c in cols]
-            if not cols[ACC_COL]:
-                # append to existing data and make it a list
-                for i in range(len(cols)):
-                    if cols[i]:
-                        working_cols[i].append(cols[i])
-                #print working_cols
-                
-                #working_cols = []
-#            str = "{}\n".format(sql_placeholders)
-#            #print cur.mogrify(str,cols)
-#            sql += cur.mogrify(str,cols)
-    sql = sql.replace('\x0b','\\r') #replace vertical tabs with carriage return marker
-    if sql:
-        fout.write(sql)
-    sql = u''
-        
-sql += "\\.\n\n"
-#print sql
-fout.write(sql)
+            # populate the hash to write out at the end
+            if cols[OBJECTID_COL] or cols[ACC_COL] or cols[TITLE_COL]:
+                objectid = cols[OBJECTID_COL]
+                if not objectid:
+                    # use the accession number
+                    objectid = cols[ACC_COL]
+                if not objectid:
+                    # um. What the heck kind of object IS this?               
+                    objectid = cols[TITLE_COL].strip().replace(' ','')
+                    sys.stderr.write("=========== WTF{}\n".format(objectid))
+            #sys.stderr.write("{}\n".format(objectid))
+            data_cols = {}
+            for i in range(len(cols)):
+                for key, val in create_tables.items():
+                    if full_col_list[i] in val:
+                        #sys.stderr.write("{} goes in {}\n".format(full_col_list[i], key))
+                        if key not in data_cols:
+                            data_cols[key] = [objectid]
+                            if key == 'wac_object':
+                                wacid=''
+                                if cols[OBJECTID_COL]:
+                                    cur.execute("select id from object where cms_id=%s",(cols[OBJECTID_COL],))
+                                    row = cur.fetchone()
+                                    if row and row['id']:
+                                        wacid = str(row['id'])
+                                data_cols[key].append(wacid)
+                            if key == 'wac_artist':
+                                wacid=''
+                                if cols[LASTNAME_COL]:
+                                    #sys.stderr.write("{} {}\n".format(cols[FIRSTNAME_COL],cols[LASTNAME_COL]))
+                                    cur.execute("select agent_id from names where first_name=%s and index_name=%s",(cols[FIRSTNAME_COL],cols[LASTNAME_COL]))
+                                    row = cur.fetchone()
+                                    if row and row['agent_id']:
+                                        wacid = str(row['agent_id'])
+                                data_cols[key].append(wacid)
+                        data_cols[key].append(cols[i])
+            
+            for key, val in data_cols.items():
+                if key not in create_data:
+                    create_data[key] = ''
+                create_data[key] += u"\t".join(val)+"\n"
+                        
+for key, val in create_tables.items():
+    sql += u"COPY {} ({}) FROM stdin;\n".format(key,','.join(val))
+    sql += create_data[key].replace('\x0b','\\r') #replace vertical tabs with carriage return marker
+    sql += "\\.\n\n"
+if sql:
+    fout.write(sql)
